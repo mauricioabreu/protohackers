fcca7e6a5b7fff65c64a09b468c8aed2e88a22c5
Maurício Antunes <mauricio.abreua@gmail.com>
2023-01-07 15:45:20 -0300

    feat: solve means to an end

---
+++ meanstoandend/README.md
@@ -0,0 +1,7 @@
+# Means To An End
+
+Problem: <https://protohackers.com/problem/2>
+
+## Useful materials
+
+* Twos complement: <https://www.youtube.com/watch?v=4qH4unVtJkE>
---
+++ meanstoandend/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+type message struct {
+	Type uint8
+	Arg1 int32
+	Arg2 int32
+}
+
+func main() {
+	port := flag.Int("port", 30001, "Specify the port run the server")
+	flag.Parse()
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		prices := map[int32]int32{}
+		go func(c net.Conn) {
+			var buf []byte
+
+			if _, err := io.ReadAtLeast(c, buf, 9); err != nil {
+				c.Write([]byte(err.Error()))
+				c.Close()
+			}
+
+			msg := decodePayload(buf)
+			switch msg.Type {
+			case 'I':
+				prices[msg.Arg1] = msg.Arg2
+			case 'Q':
+				var sum, total int32
+
+				for time, price := range prices {
+					if time >= msg.Arg1 && time <= msg.Arg2 {
+						total++
+						sum += price
+					}
+				}
+
+				if total == 0 {
+					total = 1
+				}
+
+				mean := sum / total
+				ans := make([]byte, 4)
+				binary.BigEndian.PutUint32(ans, uint32(mean))
+				c.Write(ans)
+			}
+		}(conn)
+	}
+}
+
+func decodePayload(data []byte) message {
+	return message{
+		Type: data[0],
+		Arg1: int32(binary.BigEndian.Uint32(data[1:5])),
+		Arg2: int32(binary.BigEndian.Uint32(data[5:])),
+	}
+}
